e30bdb1befea1e4d5ad9065bf88def12ac14eac2
mahmoud hashish <mahhashish@yahoo.com>
2016-04-17 02:29:50 +0200

    moving some files and changing database credentials

---
+++ login system with php and mysql - udemy.com/User-Signup-Source-Code-Changes/classes/Config.class.php
@@ -0,0 +1,15 @@
+<?php
+ 
+/**
+ * Configuration class
+ */
+
+class Config
+{
+
+  const DB_HOST = 'localhost',
+        DB_NAME = 'login_system_with_php_and_mysql_udemy',
+        DB_USER = 'root',
+        DB_PASS = 'LOflower';
+
+}
