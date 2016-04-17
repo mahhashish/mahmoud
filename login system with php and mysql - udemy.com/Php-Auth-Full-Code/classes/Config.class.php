a8d6577dc297c73aaf5e6b8ecd51442955366454
mahmoud hashish <mahhashish@yahoo.com>
2016-04-17 12:06:14 +0200

    adding full auth code to the application from udemy

---
+++ login system with php and mysql - udemy.com/Php-Auth-Full-Code/classes/Config.class.php
@@ -0,0 +1,18 @@
+<?php
+ 
+/**
+ * Configuration class
+ */
+
+class Config
+{
+
+  const DB_HOST = '',
+        DB_NAME = '',
+        DB_USER = '',
+        DB_PASS = '',
+        SMTP_HOST = '',
+        SMTP_USER = '',
+        SMTP_PASS = '';
+
+}
