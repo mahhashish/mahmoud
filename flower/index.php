9b3aa790d87708868903a6d4e70257ace1144c88
mahmoud hashish <mahhashish@yahoo.com>
2016-05-01 03:15:51 +0200

    preparing heroku--2

--- flower/index.php
+++ flower/index.php
@@ -4,4 +4,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-include_once 'flower/site/index.php';
+echo phpinfo();
+/*include_once 'site/index.php';*/
+?>
