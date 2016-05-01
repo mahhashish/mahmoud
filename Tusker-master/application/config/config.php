80a2e6d092bffdec87134aea20024d5f3c7e621b
mahmoud hashish <mahhashish@yahoo.com>
2016-05-01 15:17:15 +0200

    delete branch

--- Tusker-master/application/config/config.php
+++ Tusker-master/application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/mahmoud/CodeIgniter-3.0.6/tusker/';
+$config['base_url'] = 'http://localhost/tusker/';
 
 /*
 |--------------------------------------------------------------------------
