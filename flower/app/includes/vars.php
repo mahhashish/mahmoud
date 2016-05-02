1665120355acca0fded551e34231fabdf09ce0ce
mahmoud hashish <mahhashish@yahoo.com>
2016-05-03 01:43:31 +0200

    test

--- flower/app/includes/vars.php
+++ flower/app/includes/vars.php
@@ -1,6 +1,6 @@
 <?php 
-$db_host     = $_SERVER['DB_HOST'];
+$db_host = $_SERVER['DB_HOST'];
 $db_name = $_SERVER['DB_NAME'];
-$db_user     = $_SERVER['DB_USER'];
+$db_user = $_SERVER['DB_USER'];
 $db_pass = $_SERVER['DB_PASS'];
 ?>
