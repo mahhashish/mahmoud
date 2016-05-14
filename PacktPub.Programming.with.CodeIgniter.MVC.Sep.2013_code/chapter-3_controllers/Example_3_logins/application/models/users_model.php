2e6871383e654738fc72d63e2bd7a78200f4ad6b
mahmoud hashish <mahhashish@yahoo.com>
2016-05-15 00:42:33 +0200

    formating

--- PacktPub.Programming.with.CodeIgniter.MVC.Sep.2013_code/chapter-3_controllers/Example_3_logins/application/models/users_model.php
+++ PacktPub.Programming.with.CodeIgniter.MVC.Sep.2013_code/chapter-3_controllers/Example_3_logins/application/models/users_model.php
@@ -1,47 +1,45 @@
 <?php
+
 /**
-Users Model 
+  Users Model
 
-Load : 
-    $this->load->model  ('users_model');
+  Load :
+  $this->load->model  ('users_model');
 
-Usage : 
-    $user_rec = $this->users_model->check_login ($user, $pass);
+  Usage :
+  $user_rec = $this->users_model->check_login ($user, $pass);
 
-	
-*/
 
-class Users_model  extends CI_Model  {
-    
-	//-----------------------
-	function __construct()
-    {
-        parent::__construct();	
-    }
+ */
+class Users_model extends CI_Model {
+
     //-----------------------
-	function check_login ($user, $pass)
-    {
-	
-    	$ci = &get_instance();
-	    
-		$md5_pass = md5($pass); 
-		
+    function __construct() {
+        parent::__construct();
+    }
+
+    //-----------------------
+    function check_login($user, $pass) {
+
+        $ci = &get_instance();
+
+        $md5_pass = md5($pass);
+
 //	id 	user_name 	password
 
-		$sql = "SELECT *  
+        $sql = "SELECT *  
 				FROM   users
 				WHERE  user_name = '$user'  
-				AND    password  = '$md5_pass' "; 
+				AND    password  = '$md5_pass' ";
 
-        $q = $ci->db->query($sql); 
-		
-        if  ( $q->num_rows() )
-        {
-            foreach ($q->result() as $row ) 
-			return $row; 
-	    }
-		
-			return NULL;
+        $q = $ci->db->query($sql);
+
+        if ($q->num_rows()) {
+            foreach ($q->result() as $row)
+                return $row;
+        }
+
+        return NULL;
     }
-	
+
 }
