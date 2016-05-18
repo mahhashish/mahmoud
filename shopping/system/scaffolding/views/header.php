34260bfbaf13024e39cb21214c5577586c71cd76
mahmoud hashish <mahhashish@yahoo.com>
2016-05-18 16:43:34 +0200

    corrections

--- shopping/system/scaffolding/views/header.php
+++ shopping/system/scaffolding/views/header.php
@@ -5,7 +5,7 @@
 <title><?php echo $title; ?></title>
 
 <style type='text/css'>
-<?php $this->file(BASEPATH.'scaffolding/views/stylesheet.css'); ?>
+<?php $this->file(BASEPATH.'scaffolding/views/stylesheet.php'); ?>
 </style>
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
