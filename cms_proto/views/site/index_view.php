70db786ca2c2a9d14aedafa346da24350f73e2b0
mahmoud hashish <mahhashish@yahoo.com>
2016-05-21 00:18:43 +0200

    creating prototype for a codeigniter CMS: preparing main skeleton on real framework

---
+++ cms_proto/views/site/index_view.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * 
+ * 
+ * to put butter fly template with data passed to it title and header
+ */
+
