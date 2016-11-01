11595f26aa0cc3bc29dc95d78c761df00ccd9b4f
mahmoud hashish <gmahhashish@gmail.com>
2016-11-02 01:01:42 +0200

    --adding test file

--- test.php
+++ test.php
@@ -1,5 +1,11 @@
 <?php
-
+var_dump($_POST['user']);
+$test = ['user'];
+if ($test) {
+    echo 'true';
+} else {
+   echo 'false'; 
+}
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
